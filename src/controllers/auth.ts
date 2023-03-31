5c5785a5ec3ef1035ede1010925da0228d3340b2
02Gqbriel <eglig@bzz.ch>
2023-03-31 16:31:11 +0200

    added login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { comparePasswords } from '../util/hash';
+import { comparePasswords, hashPassword } from '../util/hash';
 
 export const router = Router();
 const upload = multer();
@@ -8,19 +8,41 @@ const upload = multer();
 const users: { [key: string]: string } = {};
 
 router.post('/login', upload.none(), async (req, res) => {
-	const { username, password } = req.body as { username: string; password: string };
+	const { username, password } = req.body as {
+		username: string;
+		password: string;
+	};
 
 	if (username == null || password == null) {
-		res.status(400).send('Invalid Body');
+		return res.status(400).send('Invalid Body');
 	}
 
 	if (!users[username]) {
-		res.status(404).send('User not found');
+		return res.status(404).send('User not found');
 	}
 
 	if (!comparePasswords(password, users[username])) {
-		res.status(400).send("Password doesn't match username");
+		return res.status(400).send("Password doesn't match username");
 	}
+
+	res.send('ok');
+});
+
+router.post('/register', upload.none(), async (req, res) => {
+	const { username, password } = req.body as {
+		username: string;
+		password: string;
+	};
+
+	if (username == null || password == null) {
+		return res.status(400).send('Invalid Body');
+	}
+
+	if (users[username]) {
+		return res.status(404).send('Username is already taken');
+	}
+
+	users[username] = hashPassword(password);
 
 	res.send('ok');
 });
