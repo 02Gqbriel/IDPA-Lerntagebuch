1c6294da16bceb4ee6588ef1df663f87588315a1
02Gqbriel <eglig@bzz.ch>
2023-05-03 15:04:20 +0200

    home page change

--- src/controllers/entry.ts
+++ src/controllers/entry.ts
@@ -1,34 +1,59 @@
-import { Router } from "express";
-import multer from "multer";
-import * as EntryDao from "../model/entryDao";
-import * as SubjectDao from "../model/subjectDao";
-import { Entry } from "../model/Entry";
-import { Subject } from "./../model/Subject";
+import { Router } from 'express';
+import multer from 'multer';
+import * as EntryDao from '../model/entryDao';
+import * as SubjectDao from '../model/subjectDao';
+import { Entry } from '../model/Entry';
+import { Subject } from './../model/Subject';
 
 export const router = Router();
 
 const upload = multer();
 
-router.post("/create", upload.none(), async (req, res) => {
-  const { title, date, subject } = req.body as {
-    title: string | undefined;
-    date: Date | undefined;
-    subject: number | undefined;
-  };
+router.post('/create', upload.none(), async (req, res) => {
+	const { title, date, subject } = req.body as {
+		title: string | undefined;
+		date: Date | undefined;
+		subject: number | undefined;
+	};
 
-  if (title == undefined || date == undefined || subject == undefined) {
-    return res.status(400).send("Invalid Body");
-  }
+	if (title == undefined || date == undefined || subject == undefined) {
+		return res.status(400).send('Invalid Body');
+	}
 
-  const subjectObject = <Subject>await SubjectDao.selectEntity(subject);
+	const subjectObject = <Subject>await SubjectDao.selectEntity(subject);
 
-  const result = await EntryDao.insertEntry(
-    new Entry(title, date, "", subjectObject)
-  );
+	const result = await EntryDao.insertEntry(
+		new Entry(title, date.toDateString(), '', subjectObject.getSubjectID())
+	);
 
-  if (typeof result == "string") {
-    return res.sendStatus(409);
-  }
+	if (typeof result == 'string') {
+		return res.sendStatus(409);
+	}
 
-  res.sendStatus(200);
+	res.sendStatus(200);
+});
+
+router.put('/update', upload.none(), async (req, res) => {
+	const { title, date, subject, id } = req.body as {
+		id: number | undefined;
+		title: string | undefined;
+		date: Date | undefined;
+		subject: number | undefined;
+	};
+
+	if (title == undefined || date == undefined || subject == undefined || ) {
+		return res.status(400).send('Invalid Body');
+	}
+
+	const subjectObject = <Subject>await SubjectDao.selectEntity(subject);
+
+	const result = await EntryDao.insertEntry(
+		new Entry(title, date.toDateString(), '', subjectObject.getSubjectID())
+	);
+
+	if (typeof result == 'string') {
+		return res.sendStatus(409);
+	}
+
+	res.sendStatus(200);
 });
--- test/controller/subject.test.ts
+++ test/controller/subject.test.ts
@@ -1,52 +1,47 @@
-import { spec } from "pactum";
-import * as SubjectDao from "../../src/model/subjectDao";
-import { string, int } from "pactum-matchers";
-import { server } from "../../src/main";
-import { Subject } from "../../src/model/Subject";
-import { token } from "./auth.test";
+import { spec } from 'pactum';
+import * as SubjectDao from '../../src/model/subjectDao';
+import { string, int } from 'pactum-matchers';
+import { server } from '../../src/main';
+import { Subject } from '../../src/model/Subject';
+import { token } from './auth.test';
 
 const PORT = process.env.PORT ?? 3001;
 
-const username = "z-user-" + Date.now();
-const password = "geheim";
-
 let subject: Subject | null = null;
 
-describe("Server | Subject", () => {
-  before(async function () {
-    if (!server.listening) {
-      server.listen(PORT);
-    }
+describe('Server | Subject', () => {
+	before(async function () {
+		if (!server.listening) {
+			server.listen(PORT);
+		}
 
-    subject = await SubjectDao.insertSubject(new Subject("Mathematik"));
+		subject = await SubjectDao.insertSubject(new Subject('Mathematik'));
+	});
 
-    console.log(subject);
-  });
+	it('Soll alle Fächer/Themen zurückbekommen', async () => {
+		if (token == null) throw new Error('Token ist null');
 
-  it("Soll alle Fächer/Themen zurückbekommen", async () => {
-    if (token == null) throw new Error("Token ist null");
+		await spec()
+			.get(`http://localhost:${PORT}/api/subject/list`)
+			.withHeaders({ authorization: token })
+			.expectStatus(200)
+			.expectHeaderContains('content-type', 'application/json');
+	});
 
-    await spec()
-      .get(`http://localhost:${PORT}/api/subject/list`)
-      .withHeaders({ authorization: token })
-      .expectStatus(200)
-      .expectHeaderContains("content-type", "application/json");
-  });
+	it('Soll ein Fächer/Themen zurückbekommen', async () => {
+		if (subject == null || token == null)
+			throw new Error('Thema oder Token ist null');
 
-  it("Soll ein Fächer/Themen zurückbekommen", async () => {
-    if (subject == null || token == null)
-      throw new Error("Thema oder Token ist null");
+		await spec()
+			.get(`http://localhost:${PORT}/api/subject/list?id=${subject}`)
+			.withHeaders({ authorization: token })
+			.expectStatus(200)
+			.expectHeaderContains('content-type', 'application/json');
+	});
 
-    await spec()
-      .get(`http://localhost:${PORT}/api/subject/list?id=${subject}`)
-      .withHeaders({ authorization: token })
-      .expectStatus(200)
-      .expectHeaderContains("content-type", "application/json");
-  });
-
-  after(() => {
-    if (server.listening) {
-      server.close();
-    }
-  });
+	after(() => {
+		if (server.listening) {
+			server.close();
+		}
+	});
 });
