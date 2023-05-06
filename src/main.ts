0e8f3675a012ae9b9150672af582b4b9b311c052
02Gqbriel <eglig@bzz.ch>
2023-05-06 17:23:19 +0200

    langs

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,72 @@
-import express from "express";
-import { router as api } from "./controllers/api";
-import { router as view } from "./controllers/view";
-import { join } from "path";
-import logger from "morgan";
-import compression from "compression";
-import { create } from "express-handlebars";
-import { createWriteStream } from "fs";
+import express from 'express';
+import { router as api } from './controllers/api';
+import { router as view } from './controllers/view';
+import { join } from 'path';
+import logger from 'morgan';
+import compression from 'compression';
+import { create } from 'express-handlebars';
+import { createWriteStream } from 'fs';
 
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT ?? 3000;
 
-const PUBLIC_FOLDER = join(process.cwd(), "src", "public");
+const PUBLIC_FOLDER = join(process.cwd(), 'src', 'public');
 
-const VIEWS_FOLDER = join(process.cwd(), "src", "views");
+const VIEWS_FOLDER = join(process.cwd(), 'src', 'views');
 
-const LOG_FOLDER = join(process.cwd(), "logs", `access.log`);
+const LOG_FOLDER = join(process.cwd(), 'logs', `access.log`);
 
-const TINYMCE = join(process.cwd(), "node_modules", "tinymce");
+const TINYMCE = join(process.cwd(), 'node_modules', 'tinymce');
+const TINYMCE_LANGS = join(
+	process.cwd(),
+	'node_modules',
+	'tinymce-i18n',
+	'langs6'
+);
 
 const app = express();
 
 app.use(
-  logger(process.env.MODE === "production" ? "combined" : "dev", {
-    stream:
-      process.env.MODE === "production"
-        ? createWriteStream(LOG_FOLDER, {
-            flags: "a",
-          })
-        : undefined,
-  })
+	logger(process.env.MODE === 'production' ? 'combined' : 'dev', {
+		stream:
+			process.env.MODE === 'production'
+				? createWriteStream(LOG_FOLDER, {
+						flags: 'a',
+				  })
+				: undefined,
+	})
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.MODE === "production") {
-  app.use(compression());
+if (process.env.MODE === 'production') {
+	app.use(compression());
 }
 
 const hbs = create({});
 
-app.engine("handlebars", hbs.engine);
+app.engine('handlebars', hbs.engine);
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
-app.set("views", VIEWS_FOLDER);
+app.set('views', VIEWS_FOLDER);
 
-if (process.env.MODE == "production") {
-  app.enable("view cache");
+if (process.env.MODE == 'production') {
+	app.enable('view cache');
 }
 
-app.use("/api", api);
+app.use('/api', api);
 
-app.use("/tinymce", express.static(TINYMCE));
+app.use('/tinymce/langs', express.static(TINYMCE_LANGS));
+
+app.use('/tinymce', express.static(TINYMCE));
 
 app.use(express.static(PUBLIC_FOLDER));
 
 app.use(view);
 
 export const server = app.listen(PORT, () => {
-  console.log("> Server running on http://127.0.0.1:3000/");
+	console.log('> Server running on http://127.0.0.1:3000/');
 });
