dfec35234c6db8f7705f9d9049ec1f89323c7fce
02Gqbriel <eglig@bzz.ch>
2023-05-07 10:47:30 +0200

    bilder weg und title ändern

--- src/public/js/tinymce-config.js
+++ src/public/js/tinymce-config.js
@@ -8,14 +8,18 @@ tinymce.init({
 		'undo redo | styles | bold italic | link image | alignleft aligncenter alignright alignjustify | outdent indent | autosave | searchreplace | restoredraft',
 	auto_focus: 'element1',
 	resize: false,
-	height: '100%',
+	height: '90%',
 	language: 'de',
 	hidden_input: true,
 	setup: function (editor) {
 		editor.on('init', async function (e) {
-			const token = sessionStorage.getItem('token');
-
 			const [id] = window.location.pathname.split('/').reverse();
+
+			if (id === 'test') {
+				return;
+			}
+
+			const token = sessionStorage.getItem('token');
 
 			const resEntry = await fetch('/api/entry/get?id=' + id, {
 				headers: { Authorization: token },
