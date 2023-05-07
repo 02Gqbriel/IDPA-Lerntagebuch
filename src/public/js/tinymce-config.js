88cdda6a6cdd16f9f9cabb0437e46c729d8f44f3
02Gqbriel <eglig@bzz.ch>
2023-05-07 11:50:26 +0200

    styling

--- src/public/js/tinymce-config.js
+++ src/public/js/tinymce-config.js
@@ -8,7 +8,7 @@ tinymce.init({
 		'undo redo | styles | bold italic | link image | alignleft aligncenter alignright alignjustify | outdent indent | autosave | searchreplace | restoredraft',
 	auto_focus: 'element1',
 	resize: false,
-	height: '90%',
+	height: '85%',
 	language: 'de',
 	hidden_input: true,
 	setup: function (editor) {
@@ -25,15 +25,31 @@ tinymce.init({
 				headers: { Authorization: token },
 			});
 
-			const { title, content } = await resEntry.json();
-
-			console.log(title);
+			const { title, content, date, subjectID } = await resEntry.json();
 
 			const titleElement = document.getElementById('title');
 
 			titleElement.innerText = title;
 
+			const dateElement = document.getElementById('date');
+
+			dateElement.innerText = date;
+
 			editor.setContent(content);
+
+			const resSubject = await fetch('/api/subject/get?id=' + subjectID, {
+				headers: { Authorization: token },
+			});
+
+			if (!resSubject) {
+				console.log(resSubject);
+			}
+
+			const json = await resSubject.json();
+
+			const subjectEl = document.getElementById('subject');
+
+			subjectEl.innerText = json.name;
 		});
 	},
 });
